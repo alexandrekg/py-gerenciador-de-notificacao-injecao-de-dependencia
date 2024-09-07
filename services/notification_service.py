003f69951bd04e6d889b62951f9b1598a219069a
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-09-07 10:54:51 -0300

    Criando o serviço de notificação

---
+++ services/notification_service.py
@@ -0,0 +1,6 @@
+class NotificationManager:
+    def __init__(self, notification_service):
+        self.notification_service = notification_service
+
+    def send_notification(self):
+        self.notification_service.send_notification()
