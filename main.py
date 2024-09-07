669f6e899d758e3dd5802a7ec771a09e4aa3565c
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-09-07 11:06:22 -0300

    Refatorando o main.py com ajustes pontuais

--- main.py
+++ main.py
@@ -5,20 +5,17 @@ from services.email_service import EmailService
 
 def _send_basic_notification():
     print('Enviando notificação para membros básico')
-    notification_service = NotificationManager(EmailService())
-    notification_service.send_notification()
+    email_service = NotificationManager(EmailService())
+    email_service.send_notification()
 
 
 def _send_premium_notification():
     print('Enviando notificação para membros premium')
-    notification_service = NotificationManager(EmailService())
-    notification_service.send_notification()
+    email_service = NotificationManager(EmailService())
+    email_service.send_notification()
 
-    notification_service = NotificationManager(SmsService())
-    notification_service.send_notification()
+    sms_service = NotificationManager(SmsService())
+    sms_service.send_notification()
 
-
-news1 = _send_basic_notification()
-news2 = _send_basic_notification()
-
-premium_news = _send_premium_notification()
+_send_basic_notification()
+_send_premium_notification()
